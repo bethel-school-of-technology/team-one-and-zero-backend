fcc74b93c674a4fbb46ba8f6e67a5f9b5504c26d
Your Name <you@example.com>
2024-05-06 21:11:01 -0500

    created a Models folder and created the User Model

---
+++ Models/UserModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace TEAM_ONE_AND_ZERO_BACKEND.Models;
+
+public class User
+{
+    [JsonIgnore]
+    public int UserId {get; set;}
+    public string? Username {get; set;}
+
+    [Required]
+    public string? Password{get; set;}
+
+    [Required]
+    [EmailAddress]
+    public string? Email {get; set;}
+
+    [Required]
+    public string? Description {get; set;}
+
+    public string? Birthdate {get; set;}
+}

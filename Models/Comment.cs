bbadddd09dad80cf6b81d0f13b67120b54716f6d
Penelope Wedemeyer <pennygracew@gmail.com>
2024-05-07 18:44:41 -0700

    Created comment model and added its necessary properties

---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TEAM_ONE_AND_ZERO_BACKEND.Models;
+
+public class Comment 
+{
+    public int CommentID { get; set; }
+
+    [Required]
+    public string? Username { get; set; }
+
+    [Required]
+    public string? Description { get; set; }
+
+}

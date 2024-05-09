26d6433df5eec0f38fb91dea12726fcd66faf580
Your Name <you@example.com>
2024-05-08 21:23:05 -0500

    Added the dbcontext with entities for both user and comment. Also injected the builder services statement for the database

---
+++ Migrations/PoPDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using TEAM_ONE_AND_ZERO_BACKEND.Models;
+
+namespace backend.Migrations;
+
+public class PoPDbContext : DbContext
+{
+    public DbSet<Comment> Comment {get; set;}
+    public DbSet<User> Users {get; set;}
+    public PoPDbContext(DbContextOptions<PoPDbContext> options)
+        : base(options)
+        {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Comment>(entity => {
+            entity.HasKey(e => e.CommentID);
+            entity.Property(e => e.Description).IsRequired();
+            entity.Property(e => e.Username).IsRequired();
+        });
+        modelBuilder.Entity<User>(entity => {
+            entity.HasKey(e => e.UserId);
+            entity.Property(e => e.Username).IsRequired();
+            entity.HasIndex(x => x.Username).IsUnique(); //making sure each user has their own unique username
+            entity.Property(e => e.Email).IsRequired();
+            entity.HasIndex(x => x.Email).IsUnique(); //making sure each user has a unique email
+            entity.Property(e => e.Password).IsRequired();
+            entity.Property(e => e.Description).IsRequired();
+            entity.Property(e => e.Birthdate);
+
+        });
+    }
+}

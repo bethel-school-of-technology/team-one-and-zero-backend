72ddbc2c7aa13c5f509364be82b4b32adf8deba3
TaysonTrihey <60018766+TaysonTrihey@users.noreply.github.com>
2024-05-09 21:39:38 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 using backend.Migrations;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var secretKey = builder.Configuration["TokenSecret"];
 
 // Add services to the container.
 
@@ -9,7 +14,31 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSqlite<PoPDbContext>("Data Source=PoPProject.db");
+
 builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = true;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ValidateLifetime = false,
+        RequireExpirationTime = false,
+        ClockSkew = TimeSpan.Zero,
+        ValidateIssuerSigningKey = true
+    };
+}
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
